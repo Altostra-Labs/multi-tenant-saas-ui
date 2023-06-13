831ddc53ac45884507bb5037e5a00b00c84afe11
Shlomi Borovitz <shlomib@altostra.com>
2023-06-13 20:13:13 +0300

    user pool update

--- Admin/src/aws-exports.ts
+++ Admin/src/aws-exports.ts
@@ -1,7 +1,7 @@
 const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_4h5inaRFi",
+    "aws_user_pools_id": "us-east-1_Vvg92XnV4",
     "aws_user_pools_web_client_id": "1hjtnpppctnvnft4cknv3j2kc1",
 };
 
